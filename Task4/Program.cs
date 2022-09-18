89b0e44d101869be29851a13d0837765b1558161
NadiaBrin <nadiabrin@gmail.com>
2022-09-18 09:31:19 +0300

    Написать программу вычисления функции Аккермана

---
+++ Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿// Написать программу вычисления функции Аккермана
+
+Console.WriteLine("Введите число m");
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число n");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine(FunctionA(m, n));
+
+int FunctionA(int m, int n)
+{
+    int result = 0;
+    if(m == 0) return result = n + 1;
+    if(m > 0 && n == 0) result = FunctionA(m - 1, 1);
+    if (m > 0 && n > 0) result = FunctionA(m - 1, FunctionA(m, n - 1));
+    return result;
+}
