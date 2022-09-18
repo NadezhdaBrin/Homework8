9f735992a9b46b09eee6fefc9ec6379070481a22
NadiaBrin <nadiabrin@gmail.com>
2022-09-18 09:16:42 +0300

    Найти сумму цифр числа

---
+++ Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿// Найти сумму цифр числа
+
+int SumNumber(int n )
+{
+    if (n == 0) return n; 
+      return n % 10 +  SumNumber(n / 10 );     
+}
+
+Console.WriteLine("Введите число: ");
+int num = int.Parse(Console.ReadLine() ?? "");
+Console.WriteLine(SumNumber(num));
