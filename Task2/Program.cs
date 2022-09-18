baa7c357250799b40a0f99893b4ec0ac47a93fdb
NadiaBrin <nadiabrin@gmail.com>
2022-09-18 08:52:10 +0300

    Найти сумму элементов от M до N, N и M заданы

---
+++ Task2/Program.cs
@@ -0,0 +1,8 @@
+﻿// Найти сумму элементов от M до N, N и M заданы
+
+int SumElements(int m, int n)
+{
+    if (m == n) return m;
+    return m + SumElements(m + 1, n);
+}
+Console.WriteLine(SumElements(1, 8));
